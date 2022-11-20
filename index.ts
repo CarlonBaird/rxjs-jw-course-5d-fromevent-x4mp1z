a8af7ab602f010a6d34c0c281101c423945700e3
CarlonBaird <carlyestrella@live.com>
2022-11-20 17:39:57 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
+import { fromEvent } from 'rxjs';
+
 const triggerButton = document.querySelector('button#trigger');
 
+fromEvent(triggerButton, 'click').subscribe({
+  next: (event) => console.log(event),
+});
