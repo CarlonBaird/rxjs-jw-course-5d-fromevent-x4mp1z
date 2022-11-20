c6d505daed229fedf723366e03cb2d5ba3428315
CarlonBaird <carlyestrella@live.com>
2022-11-20 17:47:55 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import { fromEvent } from 'rxjs';
 
 const triggerButton = document.querySelector('button#trigger');
 
-fromEvent(triggerButton, 'click').subscribe({
-  next: (event) => console.log(event),
-});
+// fromEvent(triggerButton, 'click').subscribe({
+//   next: (event) => console.log(event),
+// });
+
+fromEvent<MouseEvent>(triggerButton, 'click').subscribe((event) =>
+  console.log(event.type, event.x, event.y)
+);
